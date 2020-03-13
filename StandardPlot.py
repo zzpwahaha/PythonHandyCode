d4ee6789d3cfec5d64933ff7324204a82e5b74e1
zzpwahaha <49118945+zzpwahaha@users.noreply.github.com>
2020-03-13 17:07:33 -0600

    add fig.savefig, tight and plt.rcParams['font.family'] = "serif"

--- StandardPlot.py
+++ StandardPlot.py
@@ -1,6 +1,7 @@
 #the standard plot code for generate python-based plot
 #update in time
 plt.rcParams.update({'font.size': 16})
+plt.rcParams['font.family'] = "serif"
 plt.rcParams['font.serif'] = "Times"
 fig, ax1 = plt.subplots(figsize = [8,6])
 
@@ -14,5 +15,5 @@ ax1.minorticks_on()
 ax1.grid(which='major', linestyle='-', linewidth='0.75', color='red')
 ax1.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
 
-plt.tight_layout()
 GoldenRatio(fig)
+fig.savefig('N50_StarkDefect_Up52.pdf',bbox_inches="tight")
